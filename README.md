9820d2a936240b03c069a8b85353bb93df8d82a1
Gabe Sinkin <gsinkin@earnup.com>
2019-03-06 11:07:16 -0800

    Adding webserver with db and redis

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # devops-interview
  A webserver and background processor for the DevOps interview
+
+# Getting started
+1. `python3 -m venv venv-devops-interview`
+1. `source venv-devops-interview/bin/activate`
+1. `pip install -r requirements.txt -r test_requirements.txt`
+
+# Run server
+1. `FLASK_APP=app.py flask run`
+
+# Examples
+```
+curl http://localhost:5000/key/1234
+
+{"1234":null}
+
+curl -X POST http://localhost:5000/key/1234 --header "Content-type: application/json" --data '{"test": true}'
+
+curl http://localhost:5000/key/1234
+
+{"1234":{"test":true}}
+```
---
+++ app.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import redis
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+db = SQLAlchemy(app)
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOSTNAME", "localhost"),
+    port=os.getenv("REDIS_PORT", 6379),
+    db=0
+)
+
+
+class KeyValue(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    key_id = db.Column(db.String(80), unique=True, nullable=False)
+    value = db.Column(db.JSON, nullable=False)
+
+
+db.create_all()
+
+
+@app.route("/key/<key_id>", methods=["GET"])
+def get_value(key_id):
+    result = redis_client.get(key_id)
+    if result is None:
+        result = db.session.query(KeyValue).filter(
+            KeyValue.key_id == key_id
+        ).first().value
+    else:
+        result = json.loads(result.decode())
+    return jsonify({key_id: result if result else None})
+
+
+@app.route("/key/<key_id>", methods=["POST"])
+def set_value(key_id):
+    request_data = request.data.decode()
+    request_json = json.loads(request_data)
+    existing = db.session.query(KeyValue).filter(
+        KeyValue.key_id == key_id
+    ).first()
+    if existing:
+        existing.value = request_json
+    else:
+        db.session.add(KeyValue(key_id=key_id, value=request_json))
+
+    result = redis_client.set(key_id, request_data)
+    db.session.commit()
+    return ("", 204)
